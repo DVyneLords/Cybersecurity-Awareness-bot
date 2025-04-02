6a717feaf5d9584d16bee3a41422f84afb30473d
ST10448886 <st10448886@rcconnect.edu.za>
2025-04-02 08:15:26 +0000

    feat(validation): Implemented input validation to prevent empty queries

---
+++ Cybersecurity Awareness bot/Commits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cybersecurity_Awareness_bot
+{
+    class Add_Comments
+    {
+        static void Commits()
+        {
+            Console.WriteLine("use ASCII ");
+        }
+    }
+}
